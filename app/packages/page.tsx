30ccb1fb9e9a2d29aa528d8c93a17922c5d0d897
primaayushikumbhare25 <ayushi.kumbhare@primathon.in>
2026-02-13 16:12:23 +0530

    Add services, packages and FAQ with responsiveness

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,20 +1,160 @@
+"use client";
+import Header from "@/components/Header";
+import "./page.css";
+import { useRef } from "react";
+import Footer from "@/components/Footer";
+import QueAndAns from "@/components/QueAndAns";
+
 export default function PackagesPage() {
+  const tabsRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollTabs = (direction: "left" | "right") => {
+    if (tabsRef.current) {
+      tabsRef.current.scrollBy({
+        left: direction === "left" ? -200 : 200,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
-    <div style={styles.container}>
-      <h1 style={styles.text}>Packages</h1>
-    </div>
+    <>
+      <Header />
+
+      <div className="services-hero">
+        <h1>Core Services We Offer for Comprehensive Care</h1>
+      </div>
+      <div className="tabs-wrapper">
+
+        <div className="pricing-container">
+        <button className="circle-btn left" onClick={() => scrollTabs("left")}>
+          ❮
+        </button>
+          <div className="package-card">
+            <h3>Basic</h3>
+            <h2>
+              $99 <span>/monthly</span>
+            </h2>
+            <a href="#">Terms & Conditions</a>
+
+            <div className="divider"></div>
+
+            <h4>What's Included</h4>
+            <ul>
+              <li>All Services (Yoga, CCTV, Health etc.)</li>
+              <li>24*7 Support</li>
+              <li>Community Feature Available on All Services</li>
+            </ul>
+
+            <button className="contact-btn">Contact Us</button>
+          </div>
+
+          <div className="package-card">
+            <h3>Elite Package</h3>
+            <h2>
+              $349 <span>/monthly</span>
+            </h2>
+            <a href="#">Terms & Conditions</a>
+
+            <div className="divider"></div>
+
+            <h4>What's Included</h4>
+            <ul>
+              <li>All Services (Yoga, CCTV, Health etc.)</li>
+              <li>24*7 Support</li>
+              <li>Community Feature Available on All Services</li>
+            </ul>
+
+            <button className="contact-btn">Contact Us</button>
+          </div>
+
+          <div className="package-card">
+            <h3>Pro Package</h3>
+            <h2>
+              $199 <span>/monthly</span>
+            </h2>
+            <a href="#">Terms & Conditions</a>
+
+            <div className="divider"></div>
+
+            <h4>What's Included</h4>
+            <ul>
+              <li>All Services (Yoga, CCTV, Health etc.)</li>
+              <li>24*7 Support</li>
+              <li>Community Feature Available on All Services</li>
+            </ul>
+            <button className="contact-btn">Contact Us</button>
+          </div>
+        <button
+          className="circle-btn right"
+          onClick={() => scrollTabs("right")} 
+        >
+          ❯
+        </button>
+        </div>
+        <QueAndAns/>
+        <Footer/>
+
+      </div>
+    </>
   );
 }
 
-const styles = {
-  container: {
-    height: "80vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+
+const packages = [
+  {
+    title: "Basic",
+    price: "$99",
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
   },
-  text: {
-    fontSize: "32px",
-    fontWeight: "600",
+  {
+    title: "Elite Package",
+    price: "$349",
+    featured: true,
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
   },
-};
+  {
+    title: "Pro Package",
+    price: "$199",
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
+  },
+   {
+    title: "Basic",
+    price: "$99",
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
+  },
+  {
+    title: "Elite Package",
+    price: "$349",
+    featured: true,
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
+  },
+  {
+    title: "Pro Package",
+    price: "$199",
+    features: [
+      "All Services (Yoga, CCTV, Health etc.)",
+      "24*7 Support",
+      "Community Feature Available on All Services"
+    ]
+  }
+];
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,20 +1,138 @@
+"use client";
+import Header from "@/components/Header";
+import "./page.css";
+import { useRef } from "react";
+import Image from "next/image";
+import { Download } from "lucide-react";
+import Footer from "@/components/Footer";
+
 export default function ServicesPage() {
+  const tabsRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollTabs = (direction: "left" | "right") => {
+    if (tabsRef.current) {
+      tabsRef.current.scrollBy({
+        left: direction === "left" ? -200 : 200,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
-    <div style={styles.container}>
-      <h1 style={styles.text}>Services</h1>
-    </div>
+    <>
+      <Header />
+
+      {/* HERO */}
+      <div className="services-hero">
+        <h1>Core Services We Offer for Comprehensive Care</h1>
+      </div>
+
+      {/* BLUE INFO SECTION */}
+      <div className="services-info">
+        <p>
+          Empowering Caregivers with Seamless Solutions for Elderly Well-being
+          and Support
+        </p>
+        <button className="contact-btn">Contact Us</button>
+      </div>
+
+      {/* FILTER BUTTONS */}
+      <div className="tabs-wrapper">
+        <button className="circle-btn left" onClick={() => scrollTabs("left")}>
+          ❮
+        </button>
+
+        <div className="tabs" ref={tabsRef}>
+          <button className="tab-btn ">All Services</button>
+          <button className="tab-btn ">Security</button>
+          <button className="tab-btn ">Health</button>
+          <button className="tab-btn ">Medical</button>
+          <button className="tab-btn ">All Services</button>
+          <button className="tab-btn ">Security</button>
+          <button className="tab-btn ">Health</button>
+          <button className="tab-btn ">Medical</button>
+        </div>
+
+        <button
+          className="circle-btn right"
+          onClick={() => scrollTabs("right")}
+        >
+          ❯
+        </button>
+      </div>
+
+      {/* CARDS */}
+      <div className="services-grid">
+        {services.map((item, index) => (
+          <div className="service-card" key={index}>
+            <div
+              className="icon-box"
+              style={{ backgroundColor: item.bgColor || "#E6F0FF" }}
+            >
+              <Image src={item.image} alt={item.fees} width={60} height={60} />
+            </div>
+
+            <h3 className="service-title">{item.title}</h3>
+
+            <p className="service-desc">{item.desc}</p>
+
+            <div className="price-box">
+              <p>⭐ Service Fees: {item.fees}</p>
+              <p>⭐ Monthly cost: {item.monthly}</p>
+            </div>
+            <div className="download-btn">
+            <button><Download/>Download Prospectus</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer/>
+    </>
   );
 }
 
-const styles = {
-  container: {
-    height: "80vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+const services = [
+  {
+    title: "CCTV Camera Service Provider",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$200",
+    monthly: "$2",
+    image: "/noun-security-camera-3800770 1 (1).svg",
   },
-  text: {
-    fontSize: "32px",
-    fontWeight: "600",
+  {
+    title: "Yoga Session",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$150",
+    monthly: "$3",
+    image: "/Group 3676234.svg",
+    bgColor: "#D2E4D1",
   },
-};
+  {
+    title: "Medical Support",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$300",
+    monthly: "$5",
+    image: "/noun-security-camera-3800770 1 (1).svg",
+  },
+  {
+    title: "CCTV Camera Service Provider",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$200",
+    monthly: "$2",
+    image: "noun-security-camera-3800770 1 (1).svg",
+  },
+  {
+    title: "Yoga Session",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$150",
+    monthly: "$3",
+    image: "/Group 3676234.svg",
+    bgColor: "#D2E4D1",
+  },
+  {
+    title: "Medical Support",
+    desc: "Ensure your safety with our reliable CCTV camera services, including installation, maintenance,and upgrades.",
+    fees: "$300",
+    monthly: "$5",
+    image: "noun-security-camera-3800770 1 (1).svg",
+  },
+];
